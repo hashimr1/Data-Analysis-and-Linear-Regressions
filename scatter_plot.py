a48e6510e8f739f439107bb56b07dbca7cad766c
hashimr1 <67606070+hashimr1@users.noreply.github.com>
2020-12-11 00:10:15 -0500

    Update scatter_plot.py

--- scatter_plot.py
+++ scatter_plot.py
@@ -1,7 +1,14 @@
-"""CSC110 Fall 2020 Final assignment visualizations
+"""CSC110 Fall 2020 Final Assignment Visualizations
+
 Description
 ===============================
-This Python module contains functions that ....................
+This Python module contains functions that visualize power plant and carbon emissions data.
+
+Resources:
+https://plotly.com/python/3d-scatter-plots/
+https://plotly.com/python/scattermapbox/
+https://plotly.com/python/line-and-scatter/
+
 Copyright and Usage Information
 ===============================
 This file is provided solely for the final assignment of CSC110 at the University of Toronto
@@ -13,36 +20,222 @@ from typing import List
 
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 import data_processing
 
-power_plant_data = data_processing.read_powerplant_file('global_power_plant_database.csv', 'owid-co2-data.csv',
-                                                        'countries of the world.csv')
-emissions_data = data_processing.read_carbon_emission_file('global_power_plant_database.csv', 'owid-co2-data.csv',
+
+################################################################################
+# Create Data Frames
+################################################################################
+def power_plant_df() -> object:
+    """Create and return a data frame containing information about power plants and carbon
+    emissions for countries around the world.
+    Columns:
+        'Countries': names of all countries we have information for
+        'Emission Power Plants per Capita': number of emission power plants in a country divided by population
+        'Non-Emission Power Plants per Capita': number of emission power plants in a country divided by population
+        'Carbon Emissions per Capita': total carbon emissions divided by the population
+    """
+    power_plant_data = data_processing.read_powerplant_file('global_power_plant_database.csv', 'owid-co2-data.csv',
+                                                            'countries of the world.csv')
+    emissions_data = data_processing.read_carbon_emission_file('global_power_plant_database.csv', 'owid-co2-data.csv',
+                                                               'countries of the world.csv')
+
+    return pd.DataFrame({
+        'Countries': power_plant_data[0],
+        'Emission Power Plants per Capita': [tup[0] for tup in power_plant_data[1]],
+        'Non-Emission Power Plants per Capita': [tup[1] for tup in power_plant_data[1]],
+        'Carbon Emissions per Capita': emissions_data[1]
+    })
+
+
+def nuclear_emissions_df() -> object:
+    """Create and return a data frame containing information about nuclear power plants per capita and carbon
+    emissions for countries that have at least one nuclear power plant.
+    Columns:
+        'Countries': names of all countries that have at least one nuclear power plant
+        'Nuclear Power Plants per Capita': number of nuclear power plants in a country divided by population
+        'Carbon Emissions per Capita': total carbon emissions divided by the population
+    """
+    nuclear_data = data_processing.read_nuclear_powerplant('global_power_plant_database.csv', 'owid-co2-data.csv',
                                                            'countries of the world.csv')
-nuclear_data = data_processing.read_nuclear_powerplant('global_power_plant_database.csv', 'owid-co2-data.csv',
-                                                       'countries of the world.csv')
-nuclear_emissions = data_processing.read_nuclear_powerplant_co2('global_power_plant_database.csv', 'owid-co2-data.csv',
+    emission_data = data_processing.read_nuclear_powerplant_co2('global_power_plant_database.csv', 'owid-co2-data.csv',
                                                                 'countries of the world.csv')
-nuclear_position = data_processing.get_longtitude_latitude('global_power_plant_database.csv', 'owid-co2-data.csv',
-                                                           'countries of the world.csv')
 
-emissions_capita = [tup[0] for tup in power_plant_data[1]]
-non_emissions_capita = [tup[1] for tup in power_plant_data[1]]
-total_carbon_emissions = emissions_data[1]
-
-powerplantdf = pd.DataFrame({
-    'Emission Power Plants per Capita': emissions_capita,
-    'Non-Emission Power Plants per Capita': non_emissions_capita,
-    'Carbon Emissions per Capita': total_carbon_emissions,
-})
-
-nuclearplantdf = pd.DataFrame({
-    'Nuclear Power Plants per Capita': nuclear_data[1],
-    'Carbon Emissions per Capita': nuclear_emissions[1]
-})
+    return pd.DataFrame({
+        'Countries': nuclear_data[0],
+        'Nuclear Power Plants per Capita': nuclear_data[1],
+        'Carbon Emissions per Capita': emission_data[1]
+    })
 
 
+def nuclear_locations_df() -> object:
+    """Create and return a data frame containing information about nuclear power plants, carbon
+    emissions and their locations as an input to a map visualization.
+    Columns:
+        'Countries': names of countries corresponding to the plant
+        'Power Plant': power plant name
+        'Latitudes': latitudes for the power plant
+        'Longitudes': longitudes for the power plant
+        'Emissions': total carbon emissions per capita for that country
+    """
+    emissions_data = data_processing.read_nuclear_powerplant_co2('global_power_plant_database.csv',
+                                                                 'owid-co2-data.csv', 'countries of the world.csv')
+    position_data = data_processing.get_longtitude_latitude('global_power_plant_database.csv',
+                                                            'owid-co2-data.csv', 'countries of the world.csv')
+
+    return pd.DataFrame({
+        'Countries': position_data[0],
+        'Power Plant': position_data[1],
+        'Latitudes': position_data[2],
+        'Longitudes': position_data[3],
+        'Emissions': duplicate_emissions(emissions_data, position_data[0])
+    })
+
+
+################################################################################
+# Scatter Plot Map
+################################################################################
+def nuclear_position_map() -> None:
+    """Plot positions of nuclear power plants on the world map,
+    using mapbox and plotly.express.
+    Each point that is plotted on the map represents a nuclear power plant. The size and color of the point
+    depend on the country's carbon emissions per capita.
+
+    Documentation available at https://plotly.com/python/scattermapbox/
+    """
+    px.set_mapbox_access_token(open(".mapbox_token").read())
+
+    mapdf = nuclear_locations_df()
+
+    fig = px.scatter_mapbox(mapdf, lat='Latitudes', lon='Longitudes',
+                            template='seaborn',
+                            zoom=1.5,
+                            size='Emissions',
+                            color='Emissions',
+                            color_continuous_scale=px.colors.sequential.Jet,
+                            hover_name='Countries',
+                            hover_data=['Power Plant'],
+                            title='Nuclear Power plants around the World')
+    fig.show()
+
+
+################################################################################
+# Scatter Plots and Linear Regressions
+################################################################################
+def nuclear_emissions_plot() -> None:
+    """Plot nuclear emissions per capita and carbon emissions per capita on a scatter plot.
+    Then add a linear regression, using regression fit in regression.py.
+    Each point that is plotted represents a country. The size of the point
+    depend on the country's carbon emissions per capita.
+
+    Documentation available at https://plotly.com/python/line-and-scatter/
+    """
+    nuclearplantdf = nuclear_emissions_df()
+
+    fig = px.scatter(nuclearplantdf, x='Nuclear Power Plants per Capita',
+                     y='Carbon Emissions per Capita',
+                     color='Countries',
+                     size='Carbon Emissions per Capita',
+                     title='Carbon Emissions and Nuclear Power Plants per Capita',
+                     template='seaborn')
+
+    # # line of best fit using our linear regression
+    # our_y1 = calculate_coeff(0, , )
+    # our_y2 = calculate_coeff(6e-07, , )
+    # fig.add_trace(go.Scatter(x=[0, 6e-07], y=[our_y1, our_y2], mode="lines",
+    #                          line=go.scatter.Line(color=px.colors.qualitative.Pastel[0])))
+    #
+    # # line of best fit using sklearn's multiple linear regression
+    # sk_y1 = calculate_coeff(0, , )
+    # sk_y2 = calculate_coeff(6e-07, , )
+    # fig.add_trace(go.Scatter(x=[0, 6e-07], y=[sk_y1, sk_y2], mode="lines",
+    #                          line=go.scatter.Line(color=px.colors.qualitative.Pastel[5])))
+
+    fig.show()
+
+
+def emissions_power_plants_plot() -> None:
+    """Plot emissions power plants per capita and carbon emissions per capita on a scatter plot.
+    Then add a linear regression, using regression fit in regression.py.
+    Each point that is plotted represents a country.
+
+    Documentation available at https://plotly.com/python/line-and-scatter/
+    """
+    powerplantdf = power_plant_df()
+
+    fig = px.scatter(powerplantdf, x='Emission Power Plants per Capita',
+                     y='Carbon Emissions per Capita',
+                     title='Carbon Emissions and Emission Power Plants per Capita',
+                     template='seaborn')
+    # # line of best fit using our linear regression
+    # our_y1 = calculate_coeff(0, , )
+    # our_y2 = calculate_coeff(6e-07, , )
+    # fig.add_trace(go.Scatter(x=[0, 6e-07], y=[our_y1, our_y2], mode="lines",
+    #                          line=go.scatter.Line(color=px.colors.qualitative.Pastel[0])))
+    #
+    # # line of best fit using sklearn's multiple linear regression
+    # sk_y1 = calculate_coeff(0, , )
+    # sk_y2 = calculate_coeff(6e-07, , )
+    # fig.add_trace(go.Scatter(x=[0, 6e-07], y=[sk_y1, sk_y2], mode="lines",
+    #                          line=go.scatter.Line(color=px.colors.qualitative.Pastel[5])))
+
+    fig.show()
+
+
+def non_emissions_power_plants_plot() -> None:
+    """Plot non-emissions power plants per capita and carbon emissions per capita on a scatter plot.
+    Then add a linear regression, using regression fit in regression.py.
+    Each point that is plotted represents a country.
+
+    Documentation available at https://plotly.com/python/line-and-scatter/
+    """
+    powerplantdf = power_plant_df()
+
+    fig = px.scatter(powerplantdf, x='Non-Emission Power Plants per Capita',
+                     y='Carbon Emissions per Capita',
+                     title='Carbon Emissions and Non-Emission Power Plants per Capita',
+                     template='seaborn')
+    # # line of best fit using our linear regression
+    # our_y1 = calculate_coeff(0, , )
+    # our_y2 = calculate_coeff(6e-07, , )
+    # fig.add_trace(go.Scatter(x=[0, 6e-07], y=[our_y1, our_y2], mode="lines",
+    #                          line=go.scatter.Line(color=px.colors.qualitative.Pastel[0])))
+    #
+    # # line of best fit using sklearn's multiple linear regression
+    # sk_y1 = calculate_coeff(0, , )
+    # sk_y2 = calculate_coeff(6e-07, , )
+    # fig.add_trace(go.Scatter(x=[0, 6e-07], y=[sk_y1, sk_y2], mode="lines",
+    #                          line=go.scatter.Line(color=px.colors.qualitative.Pastel[5])))
+
+    fig.show()
+
+
+################################################################################
+# 3D Scatter Plots and Regression Surface
+################################################################################
+def powerplants_and_emissions_plot() -> None:
+    """Plot emission and non-emissions power plants per capita to prediict carbon emissions per capita
+    on a 3D scatter plot.
+    Then add a regression regression surface.
+    Each point that is plotted represents a country.
+
+    Documentation available at https://plotly.com/python/3d-scatter-plots/
+    """
+    powerplantdf = power_plant_df()
+
+    fig = px.scatter_3d(powerplantdf, x='Emission Power Plants per Capita',
+                        y='Non-Emission Power Plants per Capita',
+                        z='Carbon Emissions per Capita',
+                        title='Carbon Emissions and Type of Power Plants per Capita',
+                        template='seaborn')
+    fig.show()
+
+
+################################################################################
+# Helper Functions
+################################################################################
 def duplicate_emissions(emission: List[List], countries: List) -> List:
     """Return a list with duplicated carbon emissions corresponding with the countries that go
     with all nuclear power plants.
@@ -59,69 +252,25 @@ def duplicate_emissions(emission: List[List], countries: List) -> List:
     return emissions_so_far
 
 
-mapdf = pd.DataFrame({
-    'Countries': nuclear_position[0],
-    'Power Plant': nuclear_position[1],
-    'Latitudes': nuclear_position[2],
-    'Longitudes': nuclear_position[3],
-    'Emissions': duplicate_emissions(nuclear_emissions, nuclear_position[0])
-})
+def calculate_coeff(x_value: float, m_value: float, b_value: float) -> float:
+    """Calculate the y-value in the linear regression equation, y = mx + b
+    using the given coefficients.
+    """
+    return (m_value * x_value) + b_value
 
-# Figure 1: 3D Scatter Plot
-fig = px.scatter_3d(powerplantdf, x='Emission Power Plants per Capita',
-                    y='Non-Emission Power Plants per Capita',
-                    z='Carbon Emissions per Capita',
-                    title='Carbon Emissions and Type of Power Plants per Capita',
-                    template='seaborn')
-fig.show()
 
-# Figure 2:
-fig = px.scatter(powerplantdf, x='Emission Power Plants per Capita',
-                 y='Carbon Emissions per Capita',
-                 title='Carbon Emissions and Emission Power Plants per Capita',
-                 template='seaborn')
-fig.show()
+if __name__ == '__main__':
+    import python_ta
 
-# Figure 3:
-fig = px.scatter(powerplantdf, x='Non-Emission Power Plants per Capita',
-                 y='Carbon Emissions per Capita',
-                 title='Carbon Emissions and Non-Emission Power Plants per Capita',
-                 template='seaborn')
-fig.show()
+    python_ta.check_all(config={
+        'max-line-length': 120,
+        'extra-imports': ['python_ta.contracts', 'pandas', 'plotly.express', 'data_processing'],
+        'disable': ['R1705', 'C0200'],
+    })
 
-# Figure 4:
-fig = px.scatter(nuclearplantdf, x='Nuclear Power Plants per Capita',
-                 y='Carbon Emissions per Capita',
-                 title='Carbon Emissions and Nuclear Power Plants per Capita',
-                 template='seaborn')
-fig.show()
+    import python_ta.contracts
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
 
-# Figure 5: Map
-px.set_mapbox_access_token(open(".mapbox_token").read())
-
-fig = px.scatter_mapbox(mapdf, lat='Latitudes', lon='Longitudes',
-                        template='seaborn',
-                        zoom=1.5,
-                        size='Emissions',
-                        color='Emissions',
-                        color_continuous_scale=px.colors.sequential.Jet,
-                        hover_name='Countries',
-                        hover_data=['Power Plant'],
-                        title='Nuclear Power plants around the World')
-fig.show()
-
-# if __name__ == '__main__':
-#     import python_ta
-#
-#     python_ta.check_all(config={
-#         'max-line-length': 120,
-#         'extra-imports': ['python_ta.contracts', 'pandas', 'plotly.express', 'data_processing'],
-#         'disable': ['R1705', 'C0200'],
-#     })
-#
-#     import python_ta.contracts
-#     python_ta.contracts.DEBUG_CONTRACTS = False
-#     python_ta.contracts.check_all_contracts()
-#
-#     import doctest
-#     doctest.testmod()
+    import doctest
+    doctest.testmod()
