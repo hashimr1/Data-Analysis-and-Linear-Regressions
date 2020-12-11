57bc55f8d4b81b957d4014e10a4e1f5004e1f69a
hashimr1 <67606070+hashimr1@users.noreply.github.com>
2020-12-11 00:13:31 -0500

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -23,6 +23,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 import data_processing
+import regression
 
 
 ################################################################################
