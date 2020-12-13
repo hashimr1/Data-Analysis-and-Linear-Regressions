ae3f3661add84bb1a0fa7141e8f58fc8d02d2595
hashimr1 <67606070+hashimr1@users.noreply.github.com>
2020-12-12 20:10:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+"""CSC110 Fall 2020 Final Assignment Visualizations
+
+Description
+===============================
+Copyright and Usage Information
+===============================
+This file is provided solely for the final assignment of CSC110 at the University of Toronto
+St. George campus. All forms of distribution of this code, whether as given or with any changes,
+are expressly prohibited.
+This file is Copyright (c) 2020 Raazia Hashim, Kenneth Miura, Shilin Zhang
+
+"""
 import regression
 import data_processing
+import visualizations
 
 if __name__ == '__main__':
     # Call all the functions from our individual pieces of code
